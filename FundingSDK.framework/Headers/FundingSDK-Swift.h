7d68b19608f730880803f1cbdfa0ee6e459173f1
Olgu Sırman <olgu.sirman@vngrs.com>
2018-11-16 16:14:05 +0300

    Brand image supplied

--- FundingSDK.framework/Headers/FundingSDK-Swift.h
+++ FundingSDK.framework/Headers/FundingSDK-Swift.h
@@ -186,6 +186,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 
 
+@class UIImage;
 
 SWIFT_CLASS("_TtC10FundingSDK10CreditCard")
 @interface CreditCard : NSObject
@@ -195,6 +196,7 @@ SWIFT_CLASS("_TtC10FundingSDK10CreditCard")
 @property (nonatomic, copy) NSString * _Null_unspecified expiry;
 @property (nonatomic, copy) NSString * _Null_unspecified nameSurname;
 @property (nonatomic) BOOL requiresVerification;
+@property (nonatomic, strong) UIImage * _Nullable brandImage;
 - (NSInteger)getExpirationMonth SWIFT_WARN_UNUSED_RESULT;
 - (NSInteger)getExpiryYear SWIFT_WARN_UNUSED_RESULT;
 - (NSString * _Nonnull)getCardHolderLastName SWIFT_WARN_UNUSED_RESULT;

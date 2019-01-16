83bf5c1d290cd6fbd6eb147560b4914e70c59d97
Kagan Ozupek <kagan.ozupek@vngrs.com>
2019-01-16 17:01:10 +0300

    date

---
+++ FundingSDK.frameworkasd/Headers/FundingSDK.h
@@ -0,0 +1,19 @@
+//
+//  FundingSDK.h
+//  FundingSDK
+//
+//  Created by Kagan Ozupek on 10.10.2018.
+//  Copyright © 2018 VNGRS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FundingSDK.
+FOUNDATION_EXPORT double FundingSDKVersionNumber;
+
+//! Project version string for FundingSDK.
+FOUNDATION_EXPORT const unsigned char FundingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FundingSDK/PublicHeader.h>
+
+#import <FundingSDK/FundingRSA.h>
